f9096a79b7b649c0f9fa9d78e61d647e64a952e3
Rich Trott <rtrott@gmail.com>
2017-06-24 20:11:43 -0700

    test: refactor test-child-process-send-type-error
    * Add exit listener to child process to check return code. Previously,
      child process faiilure would not cause the test to fail.
    * Use common.mustNotCall() to guarantee callback is not invoked.
    * Insert blank line per test writing guide.
    
    PR-URL: https://github.com/nodejs/node/pull/13904
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>

--- test/parallel/test-child-process-send-type-error.js
+++ test/parallel/test-child-process-send-type-error.js
@@ -1,5 +1,6 @@
 'use strict';
-require('../common');
+const common = require('../common');
+
 const assert = require('assert');
 const cp = require('child_process');
 
@@ -13,8 +14,13 @@ function fail(proc, args) {
 
 let target = process;
 
-if (process.argv[2] !== 'child')
+if (process.argv[2] !== 'child') {
   target = cp.fork(__filename, ['child']);
+  target.on('exit', common.mustCall((code, signal) => {
+    assert.strictEqual(code, 0);
+    assert.strictEqual(signal, null);
+  }));
+}
 
 fail(target, ['msg', null, null]);
 fail(target, ['msg', null, '']);
@@ -22,4 +28,4 @@ fail(target, ['msg', null, 'foo']);
 fail(target, ['msg', null, 0]);
 fail(target, ['msg', null, NaN]);
 fail(target, ['msg', null, 1]);
-fail(target, ['msg', null, null, noop]);
+fail(target, ['msg', null, null, common.mustNotCall()]);
